1e8319a9c18e86849348ad8dd72c197ae8fbb8d8
B A <batonfack@gmail.com>
2022-05-07 21:14:52 +0200

    ExtraDextra

--- FrostysQuicksilverRancher/Patches/QuicksilverPlortCollectorPatch.cs
+++ FrostysQuicksilverRancher/Patches/QuicksilverPlortCollectorPatch.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace FrostysQuicksilverRancher.Patches
 {
-    internal class QuicksilverPlortCollector
+    class QuicksilverPlortCollectorPatch
     {
+
     }
 }
